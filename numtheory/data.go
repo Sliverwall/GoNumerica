a2e911f872cd417f65564078c3990c403de41f02
unknown <joediroberto@gmail.com>
2024-08-16 10:46:32 -0400

    Add casting method to convert []float64 and []int arrays

---
+++ numtheory/data.go
@@ -0,0 +1,27 @@
+package numtheory
+
+// Module for handling data types
+
+// ConvertIntArrToFloat64Arr takes a given []int and returns the array casted a []float64
+func ConvertIntArrToFloat64Arr(intArr []int) []float64 {
+	// Make float64 slice, set hint = to given intArr length
+	floatArr := make([]float64, len(intArr))
+	// Iterate through each element in intArray then cast as float64 and set to floatArr
+	for element := range floatArr {
+		floatArr[element] = float64(intArr[element])
+	}
+	// Return floatArr after full iteration
+	return floatArr
+}
+
+// ConvertFloat64ArrToFloat64Arr takes a given []float64 and returns the array casted a []int
+func ConvertFloat64ArrToInt64Arr(float64Arr []float64) []int {
+	// Make int slice, set hint = to given float64Arr length
+	intArr := make([]int, len(float64Arr))
+	// Iterate through each element in float64Array then cast as int and set to intArr
+	for element := range intArr {
+		intArr[element] = int(float64Arr[element])
+	}
+	// Return intArr after full iteration
+	return intArr
+}
--- numtheory/test.go
+++ numtheory/test.go
@@ -1,16 +1,28 @@
 package numtheory
 
-import "log"
+import (
+	"GoNumerica/arei"
+	"log"
+)
 
 // Test module
 
 func Test_1() {
 	// Test fib sequence
 
-	n := 10
-	figSequence := fibSeq(n)
-	log.Println(figSequence)
+	n := 20
+	fibSequence := fibSeq(n)
+	log.Println(fibSequence)
 
 	nthFib := fib(n)
 	log.Println("Fib int at n=", n, nthFib)
+
+	// Convert sequence into float64
+	floatFigSeq := ConvertIntArrToFloat64Arr(fibSequence)
+
+	figSeqArei, _ := arei.NewArei(floatFigSeq)
+	m := 4
+	figSeqArei.Reshape([]int{m, len(floatFigSeq) / m})
+	figSeqArei.Frame()
+
 }
